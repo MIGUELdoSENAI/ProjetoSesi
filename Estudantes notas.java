d2f9c9cca686e9df1972aa3dae93cbeb8841fcfd
MIGUELdoSENAI <miguel.b.maciel@aluno.senai.br>
2025-02-21 11:16:25 -0300

    commit inicial

---
+++ Estudantes notas.java
@@ -0,0 +1,25 @@
+﻿import java.util.*;
+import java.lang.Math;
+
+class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // Consultar informações do estudante
+        String nome;
+        int nota;
+
+        // Entradas
+        System.out.println("informar o nome do estudante ");
+        nome = input.nextLine();
+        System.out.println("informar a nota do estudante ");
+        nota = input.nextInt();
+
+        // Processamento
+        if (nota >= 50) {
+            System.out.println("Estudante esta aprovado ");
+        } else {
+            System.out.println("Estudante esta reprovado ");
+        }
+    }
+}
